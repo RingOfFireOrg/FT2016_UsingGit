0bc0d5d737a44d1efad657b6fcaa916da774f9ca
Alan Smith <alan@randomsmiths.com>
2016-09-30 19:56:41 -0400

    Initial set before names

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FT2016_UsingGit
+This is a simple program for RoboRio that only has an initialization.   Everyone add your name like
+```
+System.out.println("Alan Smith");
+```
---
+++ src/org/usfirst/frc/team3459/robot/Robot.java
@@ -0,0 +1,23 @@
+
+package org.usfirst.frc.team3459.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+
+/**
+ * Don't change the name of this or it won't work. (The manifest looks for "Robot")
+ */
+public class Robot extends IterativeRobot {
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	System.out.println("PT 2016-2017 members");
+    	System.out.println("--------------------");
+    	System.out.println("PT 2016-2017 mentors");
+     	System.out.println("--------------------"); 	
+    }
+        
+}
